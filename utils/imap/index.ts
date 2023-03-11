d77460cbd9d945e45fc50cf9aa8035583567a352
nabi <nabi@scvsoft.net>
2023-03-12 00:21:36 +0900

    use Class instead of function

--- utils/imap/index.ts
+++ utils/imap/index.ts
@@ -1,21 +1,42 @@
-import Imap from "imap";
+import ImapModule from "imap";
 
-if (!process.env.GMAIL_USER) throw new Error("GMAIL_USER not set");
-if (!process.env.GMAIL_PASS) throw new Error("GMAIL_PASS not set");
+export class Imap {
+  imap: ImapModule;
+  connected: boolean = false;
 
-const imap = new Imap({
-  user: process.env.GMAIL_USER,
-  password: process.env.GMAIL_PASS,
-  host: "imap.gmail.com",
-  port: 993,
-  tls: true,
-});
+  constructor() {
+    if (!process.env.GMAIL_USER) throw new Error("GMAIL_USER not set");
+    if (!process.env.GMAIL_PASS) throw new Error("GMAIL_PASS not set");
+    this.imap = new ImapModule({
+      user: process.env.GMAIL_USER,
+      password: process.env.GMAIL_PASS,
+      host: "imap.gmail.com",
+      port: 993,
+      tls: true,
+    });
+  }
 
-export function getImap(): Promise<Imap> {
-  return new Promise((resolve, reject) => {
-    imap.once("ready", () => resolve(imap));
-    imap.once("error", (err: Error) => reject(err));
-    imap.once("end", () => console.log("Connection ended"));
-    imap.connect();
-  });
+  async connect(): Promise<void> {
+    if (this.connected) return console.log("Already connected");
+    return new Promise((resolve, reject) => {
+      this.imap.once("ready", () => {
+        this.connected = true;
+        resolve();
+      });
+      this.imap.once("error", (err: Error) => reject(err));
+      this.imap.connect();
+    });
+  }
+
+  async disconnect(): Promise<void> {
+    if (!this.connected) throw new Error("Not connected");
+    return new Promise((resolve, reject) => {
+      this.imap.once("end", () => {
+        this.connected = false;
+        resolve();
+      });
+      this.imap.once("error", (err: Error) => reject(err));
+      this.imap.end();
+    });
+  }
 }
